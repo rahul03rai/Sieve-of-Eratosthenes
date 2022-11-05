d90e070b01e5b074ee3bdef280a70070aac8212b
rahul03rai <43196273+rahul03rai@users.noreply.github.com>
2022-11-06 00:31:58 +0530

    Add files via upload

---
+++ count1s.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countOnes(int arr[], int n)
+{
+    int low=0, high=n-1;
+    while(low<=high)
+    {
+        int mid=(low+high/2);
+
+        if(arr[mid==0])
+            low=mid+1;
+        else
+        {
+            if(mid==0 || arr[mid-1]==0)
+                return n-mid;
+            else    
+                high=mid-1;
+        }
+
+    }
+    return 0;
+}
+
+int main()
+{
+    int arr[5]={0, 1, 1, 1, 1}, n=5;
+    cout<<countOnes(arr, n);
+    return 0;
+}
