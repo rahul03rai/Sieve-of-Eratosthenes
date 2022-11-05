f407e942c83aadd65e3055cc05730f1280e61b9f
rahul03rai <43196273+rahul03rai@users.noreply.github.com>
2022-11-05 23:33:27 +0530

    Update cf.cpp

--- cf.cpp
+++ cf.cpp
@@ -3,27 +3,24 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
-
+    int n;cin>>n;
     int arr[n];
-    for(int i=0;i<n;i++)
+    for(int i=1;i<=n;i++)
     {
         cin>>arr[i];
     }
     int diff=INT_MAX;
-    int a, b;
-    for(int i=0;i<n;i++)
+    int ans;
+    for(int i=1;i<n;i++)
     {
-        for(int j=i+1;j<n;j++)
-        {
-            if(abs(arr[i]-arr[j])<diff){
-                diff=abs(arr[i]-arr[j]);
-                a=i;
-                b=j;}
+        if(abs(arr[i]-arr[i+1])<diff){
+            diff=abs(arr[i]-arr[i+1]);
+            ans=i;
         }
     }
-    cout<<a+1<<" "<<b+1<<endl;
-
+    if(abs(arr[n]-arr[1])<diff)
+        cout<<n<<" 1"<<endl;
+    else    
+        cout<<ans<<" "<<ans+1<<endl;
     return 0;
 }
