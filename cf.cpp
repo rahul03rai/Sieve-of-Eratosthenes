3b7142e390c6281e488105fae8789ee34b773322
rahul03rai <43196273+rahul03rai@users.noreply.github.com>
2022-11-06 00:23:37 +0530

    Add files via upload

--- cf.cpp
+++ cf.cpp
@@ -1,26 +1,62 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
+int first(int arr[], int n, int x)
 {
-    int n;cin>>n;
-    int arr[n];
-    for(int i=1;i<=n;i++)
+    int low=0, high=n-1;
+    while(low<=high)
     {
-        cin>>arr[i];
-    }
-    int diff=INT_MAX;
-    int ans;
-    for(int i=1;i<n;i++)
-    {
-        if(abs(arr[i]-arr[i+1])<diff){
-            diff=abs(arr[i]-arr[i+1]);
-            ans=i;
+        int mid=(low+high)/2;
+
+        if(x>arr[mid])
+            low=mid+1;
+        else if(x<arr[mid])
+            high=mid-1;
+        else{
+            if(mid==0 || arr[mid-1]!=arr[mid])
+                return mid;
+            else    
+                high=mid-1;
         }
     }
-    if(abs(arr[n]-arr[1])<diff)
-        cout<<n<<" 1"<<endl;
-    else    
-        cout<<ans<<" "<<ans+1<<endl;
+    return -1;
+}
+
+int last(int arr[], int n, int x)
+{
+    int low=0, high =n-1;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(x>arr[mid])
+            low=mid+1;
+        else if(x<arr[mid])
+            high=mid-1;
+        else{
+            if(mid==n-1 || arr[mid+1]!=arr[mid])
+                return mid;
+            else    
+                low=mid+1;
+        }
+
+    }
+    return -1;
+}
+
+int count(int arr[], int n, int x)
+{
+    int f=first(arr, n, x);
+
+    if(f==-1)
+        return 0;
+    else
+        return last(arr, n, x)-f+1;
+}
+
+int main()
+{
+    int arr[]={10, 20, 20, 20, 40, 40}, n=6;
+    int x=40;
+    cout<<count(arr, n, x);
     return 0;
 }
