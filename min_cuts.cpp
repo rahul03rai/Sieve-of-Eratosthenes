46d66e988114e4faceea86294c9e60318bf7ac63
rahul03rai <43196273+rahul03rai@users.noreply.github.com>
2022-11-26 21:31:17 +0530

    Update min_cuts.cpp

---
+++ min_cuts.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfCuts(int n) {
+        int ans=0;
+        if(n==1)
+            ans=0;
+        else if(n%2==0)
+            ans=n/2;
+        else
+            ans=n;
+        return ans;
+
+    }
+};
