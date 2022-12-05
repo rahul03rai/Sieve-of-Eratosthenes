8c9a3415ae1b66631c04b98aab642c3b07ebfdba
rahul03rai <43196273+rahul03rai@users.noreply.github.com>
2022-12-05 21:58:52 +0530

    Add files via upload

---
+++ 1365.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> a(101);
+        int n=nums.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            a[nums[i]]++;
+        }
+        vector<int>b;
+        
+        for(int i=0;i<n;i++)
+        {
+            int total=0;
+            for(int j=0;j<nums[i];j++)
+            {
+                total+=a[j];
+                
+            }
+            b.push_back(total);
+        }
+        return b;
+    }
+};
