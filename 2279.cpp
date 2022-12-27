0a277d9b8f262a377114f6c128a890ab73e79d60
rahul03rai <43196273+rahul03rai@users.noreply.github.com>
2022-12-27 15:48:46 +0530

    Add files via upload

---
+++ 2279.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        int ans=0;
+        for(int i=0;i<capacity.size();i++)
+        {
+            capacity[i]=capacity[i]-rocks[i];
+        }
+        sort(capacity.begin(), capacity.end());
+
+        for(auto x:capacity)
+        {
+            if(additionalRocks>=x)
+            {
+                ans++;
+                additionalRocks-=x;
+            }
+        }
+        return ans;
+    }
+};
