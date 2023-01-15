f8eaa6f9416467e1faef0836b3a44c0f56894cee
rahul03rai <43196273+rahul03rai@users.noreply.github.com>
2023-01-15 08:58:46 +0530

    Add files via upload

---
+++ 1486.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        vector<int> nums;
+        nums.push_back(start);
+        int ans=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            nums.push_back(start+2*i);
+            ans=ans^(start+2*i);
+        }
+        return ans;
+    }
+};
