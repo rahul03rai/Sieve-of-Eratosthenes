381cb41c0ac18e5b4d8ce4a919c2fa519bb24753
rahul03rai <43196273+rahul03rai@users.noreply.github.com>
2023-02-02 21:46:03 +0530

    Add files via upload

--- intersection.cpp
+++ intersection.cpp
@@ -1,31 +1,19 @@
-#include<bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> ans;
 
-void intersection(int a[], int b[], int m, int n)
-{
-    int i=0, j=0;
-
-    while(i<m && j<n)
-    {
-        if(i>0 && a[i]==a[i-1]){
-            i++;
-            continue;
-            }
-        else if(a[i]<b[i])
-            i++;
-        else if(a[i]>b[i])
-            j++;
-        else{
-            cout<<a[i]<<" ";
-            i++;j++;}
+        for(int i=0;i<nums1.size();i++)
+            ans.insert(nums1[i]);
+        vector<int>res;
+        for(int j=0;j<nums2.size();j++)
+        {
+           if(ans.find(nums2[j])!=ans.end())
+           {
+               res.push_back(nums2[j]);
+               ans.erase(nums2[j]);
+           } 
+        }
+        return res;
     }
-}
-
-int main()
-{
-    int m=5, n=4;
-    int a[m]={10, 20, 20, 40, 60};
-    int b[n]={20, 10, 5, 20};
-    intersection(a, b, m, n);
-    return 0;
-}
+};
